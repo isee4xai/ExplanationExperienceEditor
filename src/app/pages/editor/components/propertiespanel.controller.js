06989a439605fd38718035f337a10bacd0da472c
ipetyo <ipetyo@ucm.es>
2022-05-26 15:35:23 +0200

    Comments properties

--- src/app/pages/editor/components/propertiespanel.controller.js
+++ src/app/pages/editor/components/propertiespanel.controller.js
@@ -59,7 +59,8 @@
                     description: vm.original.description,
                     properties: tine.merge({}, vm.original.properties)
                 };
-
+                //  check if the property that is selected to define its values ​​in the properties component
+                //  is the explain method and the evaluate method or intends
                 switch (vm.original.name) {
                     case "Explanation Method":
                         vm.TitleName = vm.original.name;
@@ -116,7 +117,9 @@
         function UpdateProperties(option) {
             //update Explanation and Evaluation method properties
             update();
+            console.log(option);
             var json = {};
+            //we verify if the selected data have properties
             if (!option.hasOwnProperty('properties')) {
                 vm.block = {
                     title: option.value,
@@ -166,22 +169,24 @@
 
         function update() {
             //update Explanation and Evaluation method properties
-
             var p = $window.editor.project.get();
             var t = p.trees.getSelected();
             t.blocks.update(vm.original, vm.block);
 
+            // verify that we have data from the Explanation and Evaluation method
             if (vm.TitleSelect.length != null) {
                 for (let index = 0; index < vm.TitleSelect.length; index++) {
+                    //verify that the option selected in the properties title is in that data
                     if (vm.TitleSelect[index].value == vm.block.title) {
+                        //we verify that the selected data have properties
                         if (vm.TitleSelect[index].properties != null) {
                             var json = {};
-
+                            var ArrayPropertie = [];
+                            // extract the properties keys and value in objects
                             var PropertiesValue = Object.values(vm.block.properties);
                             var Propertieskey = Object.keys(vm.block.properties);
-
-                            var ArrayPropertie = [];
-                            for (let index = 0; index < Object.keys(vm.block.properties).length; index++) {
+                            //loop through all the properties and insert them into an array
+                            for (let index = 0; index < PropertiesValue.length; index++) {
                                 var JsonPropertie = {
                                     key: Propertieskey[index],
                                     value: PropertiesValue[index],
@@ -189,11 +194,13 @@
                                 ArrayPropertie.push(JsonPropertie);
                                 json[Propertieskey[index]] = PropertiesValue[index];
                             }
+                            //We assign it to the selected property
                             vm.TitleSelect[index].properties = ArrayPropertie;
                         } else {
                             vm.block.properties = null;
                             t.blocks.update(vm.original, vm.block);
                         }
+                        //we define the description of the property
                         vm.TitleSelect[index].description = vm.block.description;
                     }
 
