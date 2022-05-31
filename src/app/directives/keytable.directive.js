d3ebe919cf5f5b6f51e53f2e7ab46f5967d01330
ipetyo <ipetyo@ucm.es>
2022-05-31 13:16:43 +0200

    Properties

--- src/app/directives/keytable.directive.js
+++ src/app/directives/keytable.directive.js
@@ -1,98 +1,101 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .directive('b3KeyTable', keytable)
-    .controller('KeyTableController', KeyTableController);
+    angular
+        .module('app')
+        .directive('b3KeyTable', keytable)
+        .controller('KeyTableController', KeyTableController);
 
-  keytable.$inject = ['$parse'];
-  function keytable($parse) {
-    var directive = {
-      require          : '^ngModel',
-      restrict         : 'EA',
-      replace          : true,
-      bindToController : true,
-      controller       : 'KeyTableController',
-      controllerAs     : 'keytable',
-      templateUrl      : 'directives/keytable.html',
-      link: link
-    };
-    return directive;
+    keytable.$inject = ['$parse'];
 
-    function link(scope, element, attrs) {
-      // get the value of the `ng-model` attribute
-      scope.keytable.heading = attrs.heading;
-      scope.keytable._onChange = $parse(attrs.ngChange);
+    function keytable($parse) {
+        var directive = {
+            require: '^ngModel',
+            restrict: 'EA',
+            replace: true,
+            bindToController: true,
+            controller: 'KeyTableController',
+            controllerAs: 'keytable',
+            templateUrl: 'directives/keytable.html',
+            link: link
+        };
+        return directive;
 
-      var variable = attrs.ngModel;
-      scope.$watch(variable, function(model) {
-        scope.keytable.reset(model);
-      });
-    }
-  }
+        function link(scope, element, attrs) {
+            // get the value of the `ng-model` attribute
+            scope.keytable.heading = attrs.heading;
+            scope.keytable._onChange = $parse(attrs.ngChange);
 
-  KeyTableController.$inject = ['$scope'];
-  function KeyTableController($scope) {
-    // HEAD //
-    var vm = this;
-    vm._onChange = null;
-    vm.model  = $scope.keytable.model || $scope.model || null;
-    vm.rows   = [];
-    vm.add    = add;
-    vm.remove = remove;
-    vm.change = change;
-    vm.reset  = reset;
-
-    _activate();
-    
-    // BODY //
-    function _activate() {
-      if (vm.model) {
-        for (var key in vm.model) {
-          add(key, vm.model[key], false);
+            var variable = attrs.ngModel;
+            scope.$watch(variable, function(model) {
+                scope.keytable.reset(model);
+            });
         }
-      } else {
-        vm.model = {};
-      }
     }
 
-    function reset(model) {
-      vm.rows = [];
-      vm.model = model;
-      _activate();
-    }
+    KeyTableController.$inject = ['$scope'];
 
-    function add(key, value, fixed) {
-      vm.rows.push({key:key, value:value, fixed:fixed===true});
-    }
+    function KeyTableController($scope) {
+        // HEAD //
+        var vm = this;
+        vm._onChange = null;
+        vm.model = $scope.keytable.model || $scope.model || null;
+        vm.rows = [];
+        vm.add = add;
+        vm.remove = remove;
+        vm.change = change;
+        vm.reset = reset;
 
-    function remove(i) {
-      vm.rows.splice(i, 1);
-    }
+        _activate();
 
-    function change() {
-      for (var key in vm.model){
-        if (vm.model.hasOwnProperty(key)){
-          delete vm.model[key];
-        }
-      }
-      for (var i=0; i<vm.rows.length; i++) {
-        var r = vm.rows[i];
-        if (! r.key) continue;
-
-        var value = r.value;
-        if (!isNaN(value) && value !== '') {
-          value = parseFloat(value);
+        // BODY //
+        function _activate() {
+            if (vm.model) {
+                for (var key in vm.model) {
+                    add(key, vm.model[key], false);
+                }
+            } else {
+                vm.model = {};
+            }
         }
 
-        vm.model[r.key] = value;
-        
-        if (vm._onChange) {
-          vm._onChange($scope);
+        function reset(model) {
+            vm.rows = [];
+            vm.model = model;
+            _activate();
         }
-      }
+
+        function add(key, value, fixed) {
+            vm.rows.push({ key: key, value: value, fixed: fixed === true });
+        }
+
+        function remove(i) {
+            vm.rows.splice(i, 1);
+            change();
+        }
+
+        function change() {
+            for (var key in vm.model) {
+                if (vm.model.hasOwnProperty(key)) {
+                    delete vm.model[key];
+                }
+            }
+            for (var i = 0; i < vm.rows.length; i++) {
+                var r = vm.rows[i];
+                if (!r.key) continue;
+
+                var value = r.value;
+                if (!isNaN(value) && value !== '') {
+                    value = parseFloat(value);
+                }
+
+                vm.model[r.key] = value;
+
+                if (vm._onChange) {
+                    vm._onChange($scope);
+                }
+            }
+        }
     }
-  }
 
 })();
