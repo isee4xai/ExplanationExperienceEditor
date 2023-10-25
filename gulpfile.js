93bc17b82a7a195f913e063bf26a3121e0bf3204
Petyo Ivov Dimitrov <ipetyo@ucm.es>
2023-10-25 19:46:27 +0200

    update 25/10/23 v5

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ var vendor_js = [
     'src/assets/libs/mousetrap.min.js',
     'bower_components/angular/angular.min.js',
     'bower_components/angular-animate/angular-animate.min.js',
+    'bower_components/angular-cookies/angular-cookies.min.js',
     'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
     'bower_components/angular-ui-router/release/angular-ui-router.min.js',
     'bower_components/sweetalert/dist/sweetalert.min.js',
